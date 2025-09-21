ee7cea853cb767a2f6c22f8a555baa7a8641a058
chonong <chonong@paypal.com>
2025-09-21 13:10:20 +0800

    fixing export

--- src/components/record-button-dock.tsx
+++ src/components/record-button-dock.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useEffect, useState, useRef, useCallback } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useRecording } from '@/hooks/use-recording'
